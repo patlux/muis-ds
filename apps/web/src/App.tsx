4348ec3c9d10baaf5613393d09203a91b91a6b86
Patrick Wozniak <email@patwoz.de>
2025-03-01 19:29:03 +0100

    fix: counter for live demo

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./index.css";
 import { MUISButton } from '@patlux/muis-core'
 
 export function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="app">
-      <MUISButton variant="contained">Hello</MUISButton>
+      <MUISButton variant="contained" onClick={() => setCount(prev => prev + 1)}>Click me: {count}</MUISButton>
     </div>
   );
 }
